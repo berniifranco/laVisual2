46dcbecc68f078fce3b967c2260f305f0017c76a
Bernardo Franco <berniifranco@gmail.com>
2022-11-18 01:06:32 -0300

    clearDB

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "bdee7200668d83",
+    "password": "10b39978",
+    "database": "heroku_e41372576585311",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bdee7200668d83",
+    "password": "10b39978",
+    "database": "heroku_e41372576585311",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   }
 }
