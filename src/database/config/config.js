4d02962101a9c3066ab0eb2aed3ba74bc5c21074
Bernardo Franco <berniifranco@gmail.com>
2022-11-18 01:04:59 -0300

    conexion db clearDB

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "lavisual",
-    "host": "127.0.0.1",
+    "username": "bdee7200668d83",
+    "password": "10b39978",
+    "database": "heroku_e41372576585311",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   },
   "test": {
