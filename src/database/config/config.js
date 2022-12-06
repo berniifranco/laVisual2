3e9e0e7c6c111fbd1c4e590f08eb094ea8d93350
Bernardo Franco <berniifranco@gmail.com>
2022-12-06 18:04:31 -0300

    variables de entorno

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "bdee7200668d83",
-    "password": "10b39978",
-    "database": "heroku_e41372576585311",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
