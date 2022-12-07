c55c78e84e7e7c42297215c243de76cf478fafba
Bernardo Franco <berniifranco@gmail.com>
2022-12-07 13:00:04 -0300

    variables de entorno

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,17 +9,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "287678_lavisual",
-    "password": "1560013735",
-    "database": "lavisual2",
-    "host": "mysql-berniifranco.alwaysdata.net",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "287678_lavisual",
-    "password": "1560013735",
-    "database": "lavisual2",
-    "host": "mysql-berniifranco.alwaysdata.net",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
