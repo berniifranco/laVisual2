a602058fcf7bfd3abd37dbd77f7869b149e64eca
Bernardo Franco <berniifranco@gmail.com>
2022-12-06 22:57:39 -0300

    conexion alwaysData

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,17 +9,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "bdee7200668d83",
-    "password": "10b39978",
-    "database": "heroku_e41372576585311",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": "287678_lavisual",
+    "password": "1560013735",
+    "database": "lavisual2",
+    "host": "mysql-berniifranco.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "bdee7200668d83",
-    "password": "10b39978",
-    "database": "heroku_e41372576585311",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": "287678_lavisual",
+    "password": "1560013735",
+    "database": "lavisual2",
+    "host": "mysql-berniifranco.alwaysdata.net",
     "dialect": "mysql"
   }
 }
